43a4f04b8d7b575de94eaacd40214236c9261802
nikmak45 <74951332+jiwoon3370@users.noreply.github.com>
2025-09-17 20:35:45 +0900

    Update 통합app.py

--- unji/통합app.py
+++ unji/통합app.py
@@ -10,7 +10,7 @@ html_files = {
     "메인 페이지": "unji/htmls/index.html",
     "조추첨": "unji/htmls/조추첨.html",
     "첸토": "unji/htmls/첸토.html"
-    "HP스택 시스템" : "unji/htmls/stack_hp.html"
+    "HP스택 시스템": "unji/htmls/stack_hp.html"
 }
 
 # 사이드바에서 페이지 선택
